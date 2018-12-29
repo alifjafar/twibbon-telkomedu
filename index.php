20399b276e95e07a75cc90c51d646c6a6d33edce
Alif Jafar <alifjafar@gmail.com>
2018-12-29 16:03:31 +0700

    First

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Frame Telkom Edu 2019</title>
+    <link rel="stylesheet" href="css/cropper.css">
+</head>
+<body>
+<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+        crossorigin="anonymous"></script>
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <div class="container">
+        <a class="navbar-brand" href="/">Telkom Edutainment 2019</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="loading-alert-file" style="display:none">
+                <div class="alert alert-warning">
+                    Sedang Memuat Gambar
+                </div>
+            </div>
+            <div class="card preview">
+                <div class="card-header">
+                    Twibbon Telkom Edu 2019
+                </div>
+                <div class="card-body">
+                    <img class="img-fluid" src="img/twibbon.png">
+                </div>
+            </div>
+
+            <div class="cropper" style="display:none">
+                <div id="cropperz">
+                    <div class="card">
+                        <div class="card-header">
+                            Sesuikan Fotomu
+                        </div>
+                        <div class="card-body">
+                            <div class="twib">
+                                <img id="image" class="img-fluid" alt="img">
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <button class="btn btn-primary pull-right btn-generate" type="button" id="button">Generate
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="loading-alert" style="display:none">
+                    <div class="alert alert-warning">
+                        Gambar Sedang di Proses !
+                    </div>
+                </div>
+                <div id="cropperx">
+                    <div class="card">
+                        <div class="card-header">
+                            Hasil
+                        </div>
+                        <div class="card-body">
+                            <div id="result"></div>
+                        </div>
+                        <div class="card-footer">
+                            <a href="/" class="btn btn-info">Reset</a>
+                            <a class="btn btn-primary float-right" id="download-button"
+                               download="twibbon-telkomedu2k18.jpg"
+                               style="display:none">Download</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Upload
+                </div>
+                <div class="card-body">
+                    <div class="form-group">
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input " id="upload" accept="image/*"/>
+                            <label class="custom-file-label" for="upload">Choose file</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+<script src="js/cropper.js" type="text/javascript"></script>
+<script src="js/cropper.common.js" type="text/javascript"></script>
+<script src="js/process.js"></script>
+<script>
+    $('#upload').on('change', function (e) {
+        var fileName = e.target.files[0].name;
+        $(this).next('.custom-file-label').html(fileName);
+    })
+</script>
+</body>
+</html>
---
+++ js/process.js
@@ -0,0 +1,131 @@
+(function () {
+
+    function getRoundedCanvas(sourceCanvas) {
+        var canvas = document.createElement('canvas');
+        var context = canvas.getContext('2d');
+        var width = sourceCanvas.width;
+        var height = sourceCanvas.height;
+        var imageTwibbon = new Image();
+
+        imageTwibbon.src = "img/twibbon.png";
+
+        canvas.width = width;
+        canvas.height = height;
+
+        context.imageSmoothingEnabled = true;
+        context.drawImage(sourceCanvas, 0, 0, width, height);
+        context.drawImage(imageTwibbon, 0, 0, width, height);
+        context.globalCompositeOperation = 'destination-in';
+        context.beginPath();
+        context.fill();
+
+
+        return canvas;
+    }
+
+    function iCropper() {
+        $(".cropper").show();
+        $("#cropperx").hide();
+        var image = document.getElementById('image');
+        var button = document.getElementById('button');
+        var downloadButton = document.getElementById('download-button');
+        var result = document.getElementById('result');
+        var croppable = false;
+        var cropper = new Cropper(image, {
+            aspectRatio: 1,
+            viewMode: 3,
+            cropperFaceImage: "img/twibbon.png",
+            dragMode: 'move',
+            autoCropArea: 0.65,
+            restore: false,
+            guides: false,
+            center: false,
+            highlight: false,
+
+            cropBoxMovable: true,
+            cropBoxResizable: true,
+            toggleDragModeOnDblclick: false,
+            ready: function () {
+                croppable = true;
+            }
+        });
+
+        function resize() {
+            var croppedCanvas = cropper.getCroppedCanvas();
+            var canvas = getRoundedCanvas(croppedCanvas);
+            console.log('resize');
+            var photo = new Image();
+            photo.src = canvas.toDataURL();
+            photo.onload = function () {
+                if (photo.width > 300) {
+                    var width = 1000,
+                        height = 1000;
+
+                    var canvas = document.createElement('canvas');
+                    var context = canvas.getContext('2d');
+                    canvas.width = width;
+                    canvas.height = height;
+
+                    context.drawImage(photo, 0, 0, width, height);
+                    context.globalCompositeOperation = 'destination-in';
+                    context.beginPath();
+                    context.fill();
+                    render(canvas);
+                }
+            }
+        }
+
+        function render(canvas) {
+            $("#cropperx").show();
+            var croppedCanvas;
+            var roundedCanvas;
+            var roundedImage;
+
+            if (!croppable) {
+                return;
+            }
+            // Crop
+            //croppedCanvas = cropper.getCroppedCanvas();
+
+            // Round
+            roundedCanvas = canvas;
+
+            // Show
+            roundedImage = document.createElement('img');
+            downloadButton.setAttribute('href', roundedCanvas.toDataURL('image/jpeg', 1.0));
+            roundedImage.src = roundedCanvas.toDataURL('image/jpeg', 1.0);
+            result.innerHTML = '';
+            result.appendChild(roundedImage);
+            $("#download-button").show();
+            $('.loading-alert').hide();
+        }
+
+        button.onclick = function () {
+            $('#cropperz').hide();
+            $('.loading-alert').show();
+            $('.btn-generate').hide(resize);
+        };
+    }
+
+    $('#upload').on('change', function () {
+        $(".preview").hide();
+        $('.loading-alert-file').show();
+        var input = this;
+        setTimeout(function () {
+            readFile(input);
+        }, 500);
+    });
+
+    function readFile(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.readAsDataURL(input.files[0]);
+            reader.onload = function (e) {
+                $('#image').attr('src', e.target.result);
+                $('.loading-alert-file').hide();
+                iCropper();
+            }
+        } else {
+        }
+    }
+})();
